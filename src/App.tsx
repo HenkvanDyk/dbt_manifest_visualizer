311a24b3b1b22f8a4aa0e05f55b7e4156e9b9bab
whiai <28673491-whiai@users.noreply.replit.com>
2023-11-14 06:19:23 +0000

    Various prettifications: Bloom, Visualization Settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { useRef, useState, useEffect } from 'react';
 import { useDisclosure } from '@chakra-ui/react'
-import { ChakraProvider, Box, Button, Center, Spinner, VStack, HStack, Text, Textarea } from '@chakra-ui/react';
-import { Menu, MenuButton, MenuList, MenuItem, MenuItemOption, MenuGroup, MenuOptionGroup, MenuDivider} from '@chakra-ui/react';
-import { BsChevronDown, BsFileEarmarkCode, BsFillFileEarmarkCodeFill, BsPlusLg } from 'react-icons/bs'
+import { ChakraProvider, Box, Button, Center, Spinner, VStack, HStack, Stack, Text, Textarea, Checkbox, Divider, Tag, Icon } from '@chakra-ui/react';
+import { Menu, MenuButton, MenuList, MenuItem, MenuItemOption, MenuGroup, MenuOptionGroup, MenuDivider } from '@chakra-ui/react';
+import {Alert, AlertIcon, AlertTitle, AlertDescription} from '@chakra-ui/react'
+import { Radio, RadioGroup } from '@chakra-ui/react'
+import { BsChevronDown, BsFileEarmarkCode, BsFillFileEarmarkCodeFill, BsPlusLg, BsEyeFill } from 'react-icons/bs'
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react'
 
 import ForceGraph3D from '3d-force-graph';
-import * as utils from '/utils';
+import * as utils from './utils';
 
 import * as THREE from 'three';
-// import { UnrealBloomPass } from 'https://unpkg.com/three/examples/jsm/postprocessing/UnrealBloomPass.js';
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
+import SpriteText from 'three-spritetext';
 
 function Graph3D() {
   const example_manifest_files = [
@@ -20,25 +22,25 @@ function Graph3D() {
     "mattermost_pr1339_target_28c6f456.json",
     "tuva.json"
   ]
-  const bloomPass = new UnrealBloomPass();
-  bloomPass.strength = 3;
-  bloomPass.radius = 1;
-  bloomPass.threshold = 0;
-  
+
   // ToDo: Add 'Diff' Manifest files.
 
   const [showing_user_manifest, setShowingUserManifest] = useState<boolean>(false);
   const [selected_manifest, setSelectedManifest] = useState(example_manifest_files[0]);
   const [manifest_data, setManifestData] = useState<any>(null);
-  const [bloom_pass_on, setBloomPassOn] = useState<boolean>(true);
+  const [viz_bloom_on, setVizBloomOn] = useState<boolean>(true);
+  const [viz_3d, setViz3d] = useState<boolean>(true);
+  const [viz_text, setVizText] = useState<boolean>(false);
+  const [viz_layout, setVizLayout] = useState<string>("force");
   const [show_info, setShowInfo] = useState<boolean>(false);
   const [info_details, setInfoDetails] = useState<string | null>(null);
   const [loading_graph, setLoadingGraph] = useState<boolean>(false);
   const [graph_data, setGraphData] = useState<any>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
-  
+
   const messageAlertModalDisclosure = useDisclosure();
   const pasteManifestModalDisclosure = useDisclosure();
+  const visualizationSettingsModalDisclosure = useDisclosure();
 
   // Manifest File -> Manifest Data
   useEffect(() => {
@@ -62,8 +64,8 @@ function Graph3D() {
     if (!manifest_data) return;
     let graphData = utils.convertManifestToGraph(manifest_data);
     setGraphData(graphData);
-  }, [manifest_data]);
-  
+  }, [manifest_data, viz_3d, viz_layout]);
+
   // Graph Data -> ForceGraph3D
   useEffect(() => {
     if (containerRef.current == null) return;
@@ -71,16 +73,15 @@ function Graph3D() {
 
     setLoadingGraph(false);
 
-    const nodeClickHandler = (node) => {
-      console.log("Hello!");
+    const nodeClickHandler = (node: any) => {
       setInfoDetails(utils.getManifestNodeDetails(node.id, manifest_data));
       setShowInfo(true);
     }
-    
+
     // Generate graph
     const Graph = ForceGraph3D()(containerRef.current)
       .graphData(graph_data)
-      .numDimensions(3)
+      .numDimensions(viz_3d ? 3 : 2)
       .onNodeClick(nodeClickHandler)
       // display
       .backgroundColor('#000003')
@@ -99,14 +100,25 @@ function Graph3D() {
       .forceEngine('d3') // default 'd3' / 'ngraph'
       // .dagMode('lr') // <- For a more traditional DAG Left-to-Right layout.
       .cooldownTicks(200)
+
+
+    if (viz_text) {
       // Label via Sprite
-      // .nodeThreeObject(node => { // As Sprites
-      //   const sprite = new SpriteText(node.label);
-      //   sprite.material.depthWrite = false; // make sprite background transparent
-      //   sprite.color = node.color;
-      //   sprite.textHeight = 3;
-      //   return sprite;
-      // });
+        Graph
+          .nodeThreeObject(node => { // As Sprites
+            const sprite = new SpriteText(node.label);
+            sprite.material.depthWrite = false; // make sprite background transparent
+            sprite.color = node.color;
+            sprite.textHeight = 3;
+            // sprite.backgroundColor = true;
+            //sprite.position = new THREE.Vector3(0,1,0);
+            sprite.position.set(0,-4,0);
+            return sprite;
+          })
+          .nodeRelSize(2) // default: 4
+          .nodeThreeObjectExtend(true)
+      
+      
       //  Label via CSS2D
       // .nodeThreeObject(node => {
       //   const nodeEl = document.createElement('div');
@@ -116,19 +128,29 @@ function Graph3D() {
       //   nodeEl.className = 'node-label';
       //   return new CSS2DObject(nodeEl);
       // })
-      // .nodeThreeObjectExtend(true)
-      
-      Graph.postProcessingComposer().addPass(bloomPass);
+        //.nodeThreeObjectExtend(true)
+    }
+    if (viz_layout == "tree-lr") {
+      Graph.dagMode('lr') // <- For a more traditional DAG Left-to-Right layout.
+    }
 
-      // fit to canvas when engine stops - only for the initial load
-      let doneFirstLayout = false;
-      Graph.onEngineStop(() => {
-        if (!doneFirstLayout) {
-          doneFirstLayout = true;
-          Graph.zoomToFit(400)
-        }
-      });
-  }, [containerRef.current, graph_data]);
+    if (viz_bloom_on) {
+      const bloomPass = new UnrealBloomPass();
+      bloomPass.strength = 3;
+      bloomPass.radius = 1;
+      bloomPass.threshold = 0;
+      Graph.postProcessingComposer().addPass(bloomPass);
+    }
+
+    // FIT to canvas when engine stops - only for the initial load
+    // let doneFirstLayout = false;
+    // Graph.onEngineStop(() => {
+    //   if (!doneFirstLayout) {
+    //     doneFirstLayout = true;
+    //     Graph.zoomToFit(400)
+    //   }
+    // });
+  }, [containerRef.current, graph_data, viz_bloom_on, viz_text]);
 
   return (
     <>
@@ -141,7 +163,7 @@ function Graph3D() {
           <MenuList>
             <MenuGroup title='Manifest Snapshots'>
               {example_manifest_files.map((filename: string) => (
-                <MenuItem key={filename} 
+                <MenuItem key={filename}
                   icon={(selected_manifest == filename && !showing_user_manifest) ? <BsFillFileEarmarkCodeFill /> : <BsFileEarmarkCode />}
                   onClick={() => {
                     setShowingUserManifest(false);
@@ -151,7 +173,7 @@ function Graph3D() {
                   Demo: {utils.prettifyFilename(filename)}
                 </MenuItem>
               ))}
-              <MenuItem icon={<BsPlusLg />} 
+              <MenuItem icon={<BsPlusLg />}
                 onClick={pasteManifestModalDisclosure.onOpen}
               >
                 Paste a dbt manifest.json
@@ -160,7 +182,7 @@ function Graph3D() {
             <MenuDivider />
             <MenuGroup title='Manifest Diffs'>
               <MenuItem>…</MenuItem>
-              <MenuItem icon={<BsPlusLg />} 
+              <MenuItem icon={<BsPlusLg />}
                 onClick={messageAlertModalDisclosure.onOpen}>
                 Paste 2 dbt manifest.json files
               </MenuItem>
@@ -168,6 +190,23 @@ function Graph3D() {
           </MenuList>
         </Menu>
         <Button onClick={pasteManifestModalDisclosure.onOpen}>Paste a Manifest File</Button>
+      </HStack>
+
+      {/* BOTTOM BAR */}
+      <HStack m={2} mb={6} position="fixed" bottom={0}>
+        <Button onClick={visualizationSettingsModalDisclosure.onOpen}>
+          <HStack spacing={1}>
+            <Icon as={BsEyeFill} />
+            <Tag variant='outline' colorScheme='blue'>{viz_3d ? "3D" : "2D"}</Tag>
+            <Tag variant='outline' colorScheme='blue'>{viz_layout} Layout</Tag>
+            <Tag variant='outline' colorScheme='blue'>Showing All</Tag>
+            <Tag variant='outline' colorScheme='blue'>Colored by Folder</Tag>
+            <Tag variant='outline' colorScheme='blue'>Labels {viz_text ? "On" : "Off"}</Tag>
+            {viz_bloom_on && (
+              <Tag variant='outline' colorScheme='blue'>Bloom On</Tag>
+            )}
+          </HStack>
+        </Button>
       </HStack>
 
       {/* LOADING SPINNER */}
@@ -185,7 +224,7 @@ function Graph3D() {
             <Box>
               {info_details || <Spinner />}
             </Box>
-            <Button onClick={() => {setShowInfo(false)}} w="100%">Close</Button>
+            <Button onClick={() => { setShowInfo(false) }} w="100%">Close</Button>
           </VStack>
         </Box>
       )}
@@ -207,13 +246,101 @@ function Graph3D() {
         onClose={pasteManifestModalDisclosure.onClose}
         onSubmittedManifestData={onSubmittedManifestData}
       />
+      <VisualizationSettingsModal
+        isOpen={visualizationSettingsModalDisclosure.isOpen}
+        onOpen={visualizationSettingsModalDisclosure.onOpen}
+        onClose={visualizationSettingsModalDisclosure.onClose}
+        viz_bloom_on={viz_bloom_on}
+        setVizBloomOn={setVizBloomOn}
+        viz_3d={viz_3d}
+        setViz3d={setViz3d}
+        viz_layout={viz_layout}
+        setVizLayout={setVizLayout}
+        viz_text={viz_text}
+        setVizText={setVizText}
+      />
+
     </>
   );
 }
 
+function VisualizationSettingsModal({
+  isOpen, onOpen, onClose,
+  viz_bloom_on, setVizBloomOn,
+  viz_3d, setViz3d,
+  viz_layout, setVizLayout,
+  viz_text, setVizText
+}): {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+  viz_bloom_on: boolean; setVizBloomOn: (on: boolean) => void;
+  viz_3d: boolean; setViz3d: (on: boolean) => void;
+  viz_layout: string; setVizLayout: (value: string) => void;
+  viz_text: boolean; setVizText: (on: boolean) => void;
+} {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Visualization Settings</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Bloom</Text>
+          <Checkbox isChecked={viz_bloom_on}
+            onChange={(e) => setVizBloomOn(e.target.checked)}
+          >
+            Bloom Effect (Disable for faster rendering)
+          </Checkbox>
+          <Divider my={2} />
+          <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Dimensions</Text>
+          <RadioGroup onChange={(value) => { setViz3d(value == '3d') }} value={viz_3d ? '3d' : '2d'}>
+            <Stack direction='row'>
+              <Radio value='3d'>3D</Radio>
+              <Radio value='2d'>2D</Radio>
+            </Stack>
+          </RadioGroup>
+          <Divider my={2} />
+          <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Layout</Text>
+          <RadioGroup onChange={(value) => { setVizLayout(value) }} value={viz_layout}>
+            <Stack direction='row'>
+              <Radio value='force'>Force Graph</Radio>
+              <Radio value='tree-lr'>Tree (LR)</Radio>
+            </Stack>
+          </RadioGroup>
+          <Divider my={2} />
+          <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Colored by</Text>
+          <Text>Folder Hierarchy</Text>
+          <Divider my={2} />
+          <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Show Labels</Text>
+          <Checkbox isChecked={viz_text}
+            onChange={(e) => setVizText(e.target.checked)}
+          >
+            Show Labels
+          </Checkbox>
+          <Divider my={2} />
+          <Text>… Todo: More Settings, like different Layouts …</Text>
+          <Divider my={2} />
+          <Alert status='warning' fontSize='sm' borderRadius={8}>
+            <AlertIcon />
+            Note: This experiment doesn't currently clear its RAM with each new layout, so if things get slow, just refresh the page. :)
+          </Alert>
+        </ModalBody>
+        <ModalFooter>
+          <HStack>
+            <Button mr={3} onClick={onClose} w='100%'>
+              Close
+            </Button>
+          </HStack>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+
 function PasteManifestModal({
   isOpen, onOpen, onClose, onSubmittedManifestData
-}) : {
+}): {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
@@ -233,12 +360,12 @@ function PasteManifestModal({
       console.log('PasteManifestModal: obj:', obj);
       if (onSubmittedManifestData) onSubmittedManifestData(obj);
       onClose();
-    } catch(err) {
+    } catch (err) {
       alert('JSON is invalid');
       console.error(err);
     }
   }
-  
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -271,7 +398,7 @@ function PasteManifestModal({
 function MessageAlertModal({
   isOpen, onOpen, onClose,
   title, message
-}) : {
+}): {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
