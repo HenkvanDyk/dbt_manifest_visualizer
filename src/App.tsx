b00f9d29a1410c09de85044290bef08ae72dd561
whiai <28673491-whiai@users.noreply.replit.com>
2023-11-30 01:18:18 +0000

    Added Traditional 2D Tree DAG Layout option

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useRef, useState, useEffect } from 'react';
 import { useDisclosure } from '@chakra-ui/react'
 import { ChakraProvider, Box, Button, Center, Spinner, VStack, HStack, Stack, Text, Textarea, Checkbox, Divider, Tag, Icon, Heading, Code, Link } from '@chakra-ui/react';
 import { Menu, MenuButton, MenuList, MenuItem, MenuItemOption, MenuGroup, MenuOptionGroup, MenuDivider } from '@chakra-ui/react';
-import {Alert, AlertIcon, AlertTitle, AlertDescription} from '@chakra-ui/react'
+import { Alert, AlertIcon, AlertTitle, AlertDescription } from '@chakra-ui/react'
 import { Radio, RadioGroup } from '@chakra-ui/react'
 import { useToast } from '@chakra-ui/react'
 import { BsChevronDown, BsFileEarmarkCode, BsFillFileEarmarkCodeFill, BsPlusLg, BsEyeFill, BsFileEarmarkDiff, BsFileEarmarkDiffFill, BsXLg, BsBoxArrowUpRight, BsArrowClockwise } from 'react-icons/bs'
@@ -30,12 +30,14 @@ function Graph3D({
   viz_3d,
   viz_size_by_centrality,
   viz_overall_quality,
-  viz_show_tests
-}:{
+  viz_show_tests,
+  viz_layout
+}: {
   viz_3d: boolean;
   viz_size_by_centrality: boolean;
   viz_overall_quality: number;
   viz_show_tests: boolean;
+  viz_layout: string;
 }) {
   const example_manifest_files = [
     "gitlab_manifest.min.json", // with tests this is ~ 9K nodes. Without tests, this is ~ 4K nodes.
@@ -48,18 +50,18 @@ function Graph3D({
   const [selected_manifest, setSelectedManifest] = useState(example_manifest_files[0]); // selected_manifest_view
   const [manifest_data, setManifestData] = useState<any>(null);
   const [viz_text, setVizText] = useState<boolean>(false);
-  const [viz_layout, setVizLayout] = useState<string>("force");
   const [show_info, setShowInfo] = useState<boolean>(false);
   const [cur_node_details, setCurNodeDetails] = useState<any | null>(null);
   const [loading_graph, setLoadingGraph] = useState<boolean>(false);
   const [graph_data, setGraphData] = useState<any>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
   const [times_graph_has_run, setTimesGraphHasRun] = useState<number>(0);
-  
+
   const toast = useToast();
 
   // Manifest File -> Manifest Data
   useEffect(() => {
+    if (!selected_manifest) return;
     setLoadingStatusText(`1. Fetching Manifest: https://dbt.gitlabdata.com/manifest.json (48.1 MB)`);
     setLoadingGraph(true);
     //fetch(`/data/example_manifests/${selected_manifest}`)
@@ -102,8 +104,8 @@ function Graph3D({
         isClosable: true,
       });
     }
-    setTimesGraphHasRun(times_graph_has_run+1);
-    
+    setTimesGraphHasRun(times_graph_has_run + 1);
+
 
     const nodeClickHandler = (node: any) => {
       setCurNodeDetails(utils.getManifestNodeDetails(node.id, manifest_data));
@@ -113,7 +115,7 @@ function Graph3D({
     // Generate graph
     const Graph = ForceGraph3D()(containerRef.current)
       .graphData(graph_data)
-      
+
       // display
       .numDimensions(viz_3d ? 3 : 2)
       // .nodeResolution(6) // default = 8
@@ -125,24 +127,24 @@ function Graph3D({
       // ix
       .onNodeClick(nodeClickHandler)
       .enableNodeDrag(false)
-      
+
       // arrow head
       .linkDirectionalArrowLength(3.5)
       .linkDirectionalArrowRelPos(1)
-      // .linkCurvature(0)
-    
-      // layout
-      // .forceEngine('d3') // default 'd3' / 'ngraph'
-      // .dagMode('lr') // <- For a more traditional DAG Left-to-Right layout.
-      // .cooldownTicks(200)
-    
+    // .linkCurvature(0)
+
+    // layout
+    // .forceEngine('d3') // default 'd3' / 'ngraph'
+    // .dagMode('lr') // <- For a more traditional DAG Left-to-Right layout.
+    // .cooldownTicks(200)
+
     if (viz_overall_quality == 0) { Graph.nodeResolution(0) }
     if (viz_overall_quality == 1) { Graph.nodeResolution(4) }
     if (viz_overall_quality == 2) { Graph.nodeResolution(6) }
     if (viz_overall_quality == 3) { Graph.nodeResolution(8) }
 
     // directional particles
-    if ([1,2].includes(viz_overall_quality)) {
+    if ([1, 2].includes(viz_overall_quality)) {
       Graph
         .linkDirectionalParticles(1) // ("value")
         .linkDirectionalParticleSpeed(0.008) //(d => d.value * 0.001);
@@ -154,27 +156,48 @@ function Graph3D({
         .linkDirectionalParticleSpeed(0.008) //(d => d.value * 0.001);
         .linkDirectionalParticleWidth(1) // Nodes are ~width(10)
     }
-    
+
+    if (viz_layout == "tree-lr") {
+      Graph.dagMode('lr') // <- For a more traditional DAG Left-to-Right layout.
+
+      // Show Text Labels
+      Graph
+        .nodeThreeObject((node: any) => { // As Sprites
+          const sprite: any = new SpriteText(node.label);
+          sprite.material.depthWrite = false; // make sprite background transparent
+          sprite.color = node.color;
+          sprite.textHeight = 3;
+          // sprite.backgroundColor = true;
+          //sprite.position = new THREE.Vector3(0,1,0);
+          sprite.position.set(0, -4, 0);
+          return sprite;
+        })
+        .nodeRelSize(2) // default: 4
+        .nodeThreeObjectExtend(true)
+
+      Graph.
+        dagLevelDistance(100)
+    }
     if (viz_size_by_centrality) {
       Graph
         .nodeRelSize(2) // default 4
-        .nodeVal((node:any) => Math.pow(node.centrality, 1.5))
+        .nodeVal((node: any) => Math.pow(node.centrality, 1.5))
     }
     if (viz_text) {
       // Label via Sprite
-        Graph
-          .nodeThreeObject((node:any) => { // As Sprites
-            const sprite:any = new SpriteText(node.label);
-            sprite.material.depthWrite = false; // make sprite background transparent
-            sprite.color = node.color;
-            sprite.textHeight = 3;
-            // sprite.backgroundColor = true;
-            //sprite.position = new THREE.Vector3(0,1,0);
-            sprite.position.set(0,-4,0);
-            return sprite;
-          })
-          .nodeRelSize(2) // default: 4
-          .nodeThreeObjectExtend(true)
+      Graph
+        .nodeThreeObject((node: any) => { // As Sprites
+          const sprite: any = new SpriteText(node.label);
+          sprite.material.depthWrite = false; // make sprite background transparent
+          sprite.color = node.color;
+          sprite.textHeight = 3;
+          // sprite.backgroundColor = true;
+          //sprite.position = new THREE.Vector3(0,1,0);
+          sprite.position.set(0, -4, 0);
+          return sprite;
+        })
+        .nodeRelSize(2) // default: 4
+        .nodeThreeObjectExtend(true)
       //  Label via CSS2D
       // .nodeThreeObject(node => {
       //   const nodeEl = document.createElement('div');
@@ -184,7 +207,7 @@ function Graph3D({
       //   nodeEl.className = 'node-label';
       //   return new CSS2DObject(nodeEl);
       // })
-        //.nodeThreeObjectExtend(true)
+      //.nodeThreeObjectExtend(true)
     }
     if (viz_layout == "tree-lr") {
       Graph.dagMode('lr') // <- For a more traditional DAG Left-to-Right layout.
@@ -219,15 +242,15 @@ function Graph3D({
       {/* TOP BAR */}
       <HStack m={4}>
         <Button
-          onClick={() => {window.open('https://large-dbt-dag-visualizer.replit.app/');}}
+          onClick={() => { window.open('https://large-dbt-dag-visualizer.replit.app/'); }}
           colorScheme="yellow"
-          // variant="outline"
-          // bgColor="rgba(0,0,0,0.8)"
+        // variant="outline"
+        // bgColor="rgba(0,0,0,0.8)"
         >
           Go to DAG Visualizer Tool <Icon as={BsBoxArrowUpRight} ml={2} />
         </Button>
         <Button
-          onClick={() => {location.reload();}}
+          onClick={() => { location.reload(); }}
           colorScheme="twitter"
           variant="outline"
           bgColor="rgba(0,0,0,0.8)"
@@ -273,20 +296,20 @@ function Graph3D({
                   <Divider my={2} />
 
                   {cur_node_details.columns && (
-                  <>
-                    <VStack spacing={0} alignItems="baseline" my={2} position="relative">
-                      <Text fontSize='sm' fontWeight="bold" textTransform="uppercase" opacity={0.6}>
-                        Columns
-                      </Text>
-                      {Object.keys(cur_node_details.columns).length == 0 && (<Text>none</Text>)}
-                      {Object.keys(cur_node_details.columns).map((key: string, index: number) => (
-                        <Text key={key}>{key}</Text>
-                      ))}
-                    </VStack>
-                    <Divider my={2} />
+                    <>
+                      <VStack spacing={0} alignItems="baseline" my={2} position="relative">
+                        <Text fontSize='sm' fontWeight="bold" textTransform="uppercase" opacity={0.6}>
+                          Columns
+                        </Text>
+                        {Object.keys(cur_node_details.columns).length == 0 && (<Text>none</Text>)}
+                        {Object.keys(cur_node_details.columns).map((key: string, index: number) => (
+                          <Text key={key}>{key}</Text>
+                        ))}
+                      </VStack>
+                      <Divider my={2} />
                     </>
                   )}
-                  
+
 
                   <VStack spacing={0} alignItems="baseline" my={2} position="relative">
                     <Text fontSize='sm' fontWeight="bold" textTransform="uppercase" opacity={0.6}>
@@ -297,8 +320,8 @@ function Graph3D({
                     </Code>
                   </VStack>
                   <Divider my={2} />
-                  
-                  
+
+
                 </>
               )}
               {!cur_node_details && (
@@ -323,63 +346,76 @@ export default function App() {
   const [viz_size_by_centrality, setVizSizeByCentrality] = useState<boolean>(true);
   const [viz_overall_quality, setVizOverallQuality] = useState<number>(2);
   const [viz_show_tests, setVizShowTests] = useState<boolean>(false);
+  const [viz_layout, setVizLayout] = useState<string>("force");
 
   useEffect(() => {
     if (viz_show_tests == true) {
       setVizOverallQuality(Math.max(viz_overall_quality - 1, 0));
     }
   }, [viz_show_tests])
-  
+
   return (
     <ChakraProvider>
       {!should_start && (
-      <Center bg='#000235' h='100vh'>
-        <Box borderRadius={6} bg='white' w='480px' maxW='100%' m={4} p={4}>
-          <Heading>GitLab DAG Visualizer</Heading>
-          <Text lineHeight='150%' my={4}>
-            This is a fork of <Link color='blue.500' href='https://large-dbt-dag-visualizer.replit.app/'>Large dbt Dag Visualizer</Link>. It's optimized for larger DAGs like GitLab (less visual effects).
-          </Text>
-          <Alert status='warning' borderRadius={4} my={4}>
-            <AlertIcon />
-            GitLab's DAG is 50mb. It is recommended to close other Tabs and CPU-heavy apps before starting the visualizer.
-          </Alert>
+        <Center bg='#000235' h='100vh'>
+          <Box borderRadius={6} bg='white' w='480px' maxW='100%' m={4} p={4}>
+            <Heading>GitLab DAG Visualizer</Heading>
+            <Text lineHeight='150%' my={4}>
+              This is a fork of <Link color='blue.500' href='https://large-dbt-dag-visualizer.replit.app/'>Large dbt Dag Visualizer</Link>. It's optimized for larger DAGs like GitLab (less visual effects).
+            </Text>
+            <Alert status='warning' borderRadius={4} my={4}>
+              <AlertIcon />
+              GitLab's DAG is 50mb. It is recommended to close other Tabs and CPU-heavy apps before starting the visualizer.
+            </Alert>
 
-          <Divider my={2} />
-          <Box className="_visualization_settings" px={4}>
-            <Text fontSize='md' fontWeight='bold' textTransform='uppercase'>Visualization Settings</Text>
             <Divider my={2} />
-            <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Dimensions</Text>
-            <RadioGroup onChange={(value) => { setViz3d(value == '3d') }} value={viz_3d ? '3d' : '2d'}>
-              <Stack direction='row'>
-                <Radio value='3d'>3D</Radio>
-                <Radio value='2d'>2D</Radio>
-              </Stack>
-            </RadioGroup>
-            <Divider my={2} />
-            <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Node Size</Text>
-            <RadioGroup 
-              onChange={(value) => { setVizSizeByCentrality(value == 'centrality') }} 
-              value={viz_size_by_centrality ? 'centrality' : 'constant'}
-            >
-              <Stack direction='row'>
-                <Radio value='centrality'># Dependencies + Children</Radio>
-                <Radio value='constant'>Constant</Radio>
-              </Stack>
-            </RadioGroup>
-            <Divider my={2} />
-            <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Overall Render Quality</Text>
-            <RadioGroup 
-              onChange={(value) => { setVizOverallQuality(Number(value)) }} 
-              value={viz_overall_quality.toString()}
-            >
-              <Stack direction='row'>
-                <Radio value='3'>High</Radio>
-                <Radio value='2'>Medium</Radio>
-                <Radio value='1'>Low</Radio>
-                <Radio value='0'>Very Low</Radio>
-              </Stack>
-            </RadioGroup>
-            {/* <Divider my={2} />
+            <Box className="_visualization_settings" px={4}>
+              <Text fontSize='md' fontWeight='bold' textTransform='uppercase'>Visualization Settings</Text>
+              <Divider my={2} />
+              <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Layout</Text>
+              <RadioGroup onChange={(value) => {
+                  setVizLayout(value); setViz3d((value == 'force')); setVizSizeByCentrality((value == 'force'))
+                }}
+                value={viz_layout}
+              >
+                <Stack direction='row'>
+                  <Radio value='force'>Force Graph</Radio>
+                  <Radio value='tree-lr'>Tree DAG (LR) (Traditional)</Radio>
+                </Stack>
+              </RadioGroup>
+              <Divider my={2} />
+              <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Dimensions</Text>
+              <RadioGroup onChange={(value) => { setViz3d(value == '3d') }} value={viz_3d ? '3d' : '2d'}>
+                <Stack direction='row'>
+                  <Radio value='3d'>3D</Radio>
+                  <Radio value='2d'>2D</Radio>
+                </Stack>
+              </RadioGroup>
+              <Divider my={2} />
+              <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Node Size</Text>
+              <RadioGroup
+                onChange={(value) => { setVizSizeByCentrality(value == 'centrality') }}
+                value={viz_size_by_centrality ? 'centrality' : 'constant'}
+              >
+                <Stack direction='row'>
+                  <Radio value='centrality'># Dependencies + Children</Radio>
+                  <Radio value='constant'>Constant</Radio>
+                </Stack>
+              </RadioGroup>
+              <Divider my={2} />
+              <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>Overall Render Quality</Text>
+              <RadioGroup
+                onChange={(value) => { setVizOverallQuality(Number(value)) }}
+                value={viz_overall_quality.toString()}
+              >
+                <Stack direction='row'>
+                  <Radio value='3'>High</Radio>
+                  <Radio value='2'>Medium</Radio>
+                  <Radio value='1'>Low</Radio>
+                  <Radio value='0'>Very Low</Radio>
+                </Stack>
+              </RadioGroup>
+              {/* <Divider my={2} />
             <Text fontSize='sm' fontWeight='bold' textTransform='uppercase'>dbt Tests</Text>
             <RadioGroup 
               onChange={(value) => { setVizShowTests(value == 'show') }} 
@@ -390,22 +426,23 @@ export default function App() {
                 <Radio value='show'>Show Test Nodes</Radio>
               </Stack>
             </RadioGroup> */}
+            </Box>
+            <Divider my={2} />
+
+            <Button onClick={() => { setShouldStart(true) }} colorScheme='blue' w='100%' mt={6} my={2}>
+              Start Visualizer
+            </Button>
           </Box>
-          <Divider my={2} />
-          
-          <Button onClick={() => {setShouldStart(true)}} colorScheme='blue' w='100%' mt={6} my={2}>
-            Start Visualizer
-          </Button>
-        </Box>
-      </Center>
-        
+        </Center>
+
       )}
       {should_start && (
-        <Graph3D 
+        <Graph3D
           viz_3d={viz_3d}
           viz_size_by_centrality={viz_size_by_centrality}
           viz_overall_quality={viz_overall_quality}
           viz_show_tests={viz_show_tests}
+          viz_layout={viz_layout}
         />
       )}
     </ChakraProvider>
